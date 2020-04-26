1527d228a1826f12424d6ccf66b1e2e2593e7194
Abhee Hudani <hudaniabhee@gmail.com>
2020-04-27 05:10:41 +0530

    Updated Task Properties

--- src/Novartus/Task.java
+++ src/Novartus/Task.java
@@ -20,10 +20,33 @@ public class Task {
 
     }*/
 
-    StringProperty priority = new SimpleStringProperty() ;
-    StringProperty description = new SimpleStringProperty() ;
-    ObjectProperty<Integer> progress = new SimpleObjectProperty<>(0);
+    private final StringProperty priority = new SimpleStringProperty();
+    private final StringProperty description = new SimpleStringProperty();
+    private final ObjectProperty<Integer> progress = new SimpleObjectProperty<>(0);
+    private final ObjectProperty<Integer> id = new SimpleObjectProperty<>(null);
 
+    public Task() {
+
+    }
+
+    public Task(Integer id, String priority, String description, Integer progress) {
+        this.id.set(id);
+        this.priority.set(priority);
+        this.description.set(description);
+        this.progress.set(progress);
+    }
+
+    public void setId(Integer value){
+        this.id.set(value);
+    }
+
+    public Integer getId(){
+      return this.id.get();
+    }
+
+    public ObjectProperty<Integer> idProperty() {
+        return id;
+    }
 
     public String getPriority() {
         return priority.get();
